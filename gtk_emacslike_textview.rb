b96570b7b91798630b0e08636461007fc3b69fe0
Takuma Nakajima <penguin.trance.2716@gmail.com>
2013-01-18 15:21:56 +0900

    add mikutter command to expand_snippet

--- gtk_emacslike_textview.rb
+++ gtk_emacslike_textview.rb
@@ -366,5 +366,16 @@ class Gtk::PostBox
 
 Plugin.create :gtk_emacslike_textview do
   on_before_postbox_post do |text|
-    Gtk::EmacsLikeTextView.pushGlobalStack(text) end end
+    Gtk::EmacsLikeTextView.pushGlobalStack(text) 
+  end
+
+  command(:expand_snippet,
+          name: 'snippetを展開',
+          condition: lambda{ |opt| true },
+          visible: true,
+          role: :postbox) do |opt|
+    Plugin.create(:gtk).widgetof(opt.widget).widget_post.expand_snippet
+  end
+
+end
 
